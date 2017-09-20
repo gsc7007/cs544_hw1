aa8422278181b66f235cf3f503e2ef2479c7f294
clttam92 <31664997+clttam92@users.noreply.github.com>
2017-09-20 05:07:05 -0700

    Commit 5

---
+++ app/src/main/java/com/example/clement/firstapp/DisplayMessageActivity.java
@@ -0,0 +1,81 @@
+package com.example.clement.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.R.attr.button;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    private TextView text1;
+    private TextView text3;
+    MainActivity newActivity = new MainActivity();
+    List<String> list = new ArrayList<>();
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+
+        StringBuilder builder = new StringBuilder();
+
+
+
+
+        List<Integer>myList = newActivity.list;
+        int listAmt = newActivity.list.size();
+        for (int i = 0; i < listAmt-1; i++) {
+
+            for (int j = 0; j < listAmt - i - 1; j++) {
+
+                for (int k = 0; k < listAmt - i; k++) {
+                        builder.append(myList.get(k));
+                        builder.append("  ");
+
+                    }
+
+
+                if (myList.get(j) > myList.get(j + 1)) {
+                    int temp = myList.get(j);
+                    myList.set(j, myList.get(j + 1)); //swaps position of j with j+1
+                    myList.set(j + 1, temp);
+
+
+                }
+                list.add(builder.toString());
+
+            }
+
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list);
+        ListView listView = (ListView) findViewById(R.id.listview);
+        listView.setAdapter(adapter);
+
+
+
+    }
+
+
+
+    @Override
+    public void onBackPressed()
+    {
+        System.exit(0);
+    }
+
+    }
+
+
--- app/src/main/java/com/example/clement/firstapp/MainActivity.java
+++ app/src/main/java/com/example/clement/firstapp/MainActivity.java
@@ -1,31 +1,30 @@
 package com.example.clement.firstapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    static List<Integer> list = new ArrayList<Integer>();
+    private Button myButton;
+    private int one;
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        myButton = (Button) findViewById(R.id.button3);
+        myButton.setOnClickListener(this); //this refers to onclick method
     }
 
     @Override
@@ -49,4 +48,155 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+    @Override
+    public void onClick(View view) {
+
+
+
+
+        Button button = (Button) findViewById(R.id.button3);
+
+
+        button.setText("x");
+
+        TextView Text1 = (TextView) findViewById(R.id.Text1);
+        TextView Text2 = (TextView) findViewById(R.id.Text2);
+        TextView Text3 = (TextView) findViewById(R.id.Text3);
+        TextView Text4 = (TextView) findViewById(R.id.Text4);
+        TextView Text5 = (TextView) findViewById(R.id.Text5);
+        TextView Text6 = (TextView) findViewById(R.id.Text6);
+        TextView Text7 = (TextView) findViewById(R.id.Text7);
+        TextView Text8 = (TextView) findViewById(R.id.Text8);
+
+        int number1 = 0;
+        String num1 = "";
+        int number2 = 0;
+        String num2 = "";
+        int number3 = 0;
+        String num3 = "";
+        int number4 = 0;
+        String num4 = "";
+        int number5 = 0;
+        String num5 = "";
+        int number6 = 0;
+        String num6 = "";
+        int number7 = 0;
+        String num7 = "";
+        int number8 = 0;
+        String num8 = "";
+
+
+        try {
+            number1 = Integer.valueOf(Text1.getText().toString());
+        }
+        catch (NumberFormatException e){
+            num1 = null;
+        } try {
+            number2 = Integer.valueOf(Text2.getText().toString());
+        }
+        catch (NumberFormatException e){
+            num2 = null;
+        } try {
+            number3 = Integer.valueOf(Text3.getText().toString());
+        }
+        catch (NumberFormatException e){
+            num3 = null;
+        } try {
+            number4 = Integer.valueOf(Text4.getText().toString());
+        }
+        catch (NumberFormatException e){
+            num4 = null;
+        } try {
+            number5 = Integer.valueOf(Text5.getText().toString());
+        }
+        catch (NumberFormatException e){
+            num5 = null;
+        } try {
+            number6 = Integer.valueOf(Text6.getText().toString());
+        }
+        catch (NumberFormatException e){
+            num6 = null;
+        } try {
+            number7 = Integer.valueOf(Text7.getText().toString());
+        }
+        catch (NumberFormatException e){
+            num7 = null;
+        } try {
+            number8 = Integer.valueOf(Text8.getText().toString());
+        }
+        catch (NumberFormatException e){
+            num8 = null;
+        }
+
+
+
+
+        if(num1 == ""){
+            list.add(number1);
+        };
+        if(num2 == ""){
+            list.add(number2);
+        };
+        if(num3 == ""){
+            list.add(number3);
+        };
+        if(num4 == ""){
+            list.add(number4);
+        };
+        if(num5 == ""){
+            list.add(number5);
+        };
+        if(num6 == ""){
+            list.add(number6);
+        };
+        if(num7 == ""){
+            list.add(number7);
+        };
+        if(num8 == ""){
+        list.add(number8);
+        };
+
+
+
+        //String t1 = Text1.toString();
+        //String tt = String.format("%s %s %s %s %s %s %s %s",number1,number2,number3,number4,number5,number6,
+        //        number7, number8);
+
+
+        int x = list.size();
+        button.setText("Sort");
+
+        TextView error = (TextView)findViewById(R.id.textView);
+
+        if(list.size()<2){
+            error.setText("Error, please enter 2 or more input");
+
+        }else if(number1<0&number1>9||number2<0||number2>9||number3<0||number3>9||number4<0||
+                number4>9||number5<0||number5>9||number6<0||number6>9||number7<0||number7>9||number8<0||number8>9
+        ){
+            error.setText("Error, input values must be between 1 and 9");
+
+        }
+        else{
+            Intent startActivity = new Intent(this, DisplayMessageActivity.class);
+            startActivity(startActivity);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+
+
+
+
